9023e55f943acb8cec5b03583a827f25c2e48533
markitus18 <marc18.garrigo@gmail.com>
2015-04-14 14:02:23 +0100

    Revert "Revert "Tree Order Recursive""
    This reverts commit 53c56a31c21f51e5cddc29d9a1ba87b8633447a4.

--- Programming project/Tree.h
+++ Programming project/Tree.h
@@ -3,39 +3,99 @@
 
 #include "DList.h"
 
-template <class TYPE>
-struct tnode
+//crear "VisitAllNodes" en recursiu i iteratiu, en tots els tipus d'ordre; en iteratiu s'utilitza una pila de crides
+template <class DATA>
+struct treeNode
 {
-	TYPE data;
-	tnode* parent;
-	DList<tnode<TYPE>> children;
+	DATA data;
+	treeNode* parent;
+	DList<treeNode<DATA>*> children;
 
-	tnode(TYPE _data)
+	treeNode(DATA _data)
 	{
 		data = _data;
 	}
+
+	void VisitAll_PreorderRecursive(DList<DATA>* list)
+	{
+		list->Add(data);
+		for (unsigned int i = 0; i < children.NodeCounter(); i++)
+		children.GetPointer(i)->atr->VisitAll_PreorderRecursive(list);
+	}
+
+	void VisitAll_PostorderRecursive(DList<DATA>* list)
+	{
+
+		for (unsigned int i = 0; i < children.NodeCounter(); i++)
+			children.GetPointer(i)->atr->VisitAll_PostorderRecursive(list);
+		list->Add(data);
+	}
+
+	void VisitAll_InorderRecursive(DList<DATA>* list)
+	{
+		for (unsigned int i = 0; i < children.NodeCounter()/2; i++)
+			children.GetPointer(i)->atr->VisitAll_InorderRecursive(list);
+		list->Add(data);
+		for (unsigned int i = children.NodeCounter() / 2; i < (children.NodeCounter()); i++)
+			children.GetPointer(i)->atr->VisitAll_InorderRecursive(list);
+	}
 };
 
-template <class TYPE>
+template <class DATA>
 class Tree
 {
-	tnode<TYPE>* rootNode;
+private:
+	treeNode<DATA>* rootNode;
 
 public:
 
-	void Add(const TYPE& dataGiven)
+	///////////////
+	//Constructor//
+	///////////////
+
+	Tree(const DATA& dataGiven)
 	{
-		tnode<TYPE>* newNode = new tnode<TYPE>(dataGiven);
-		rootNode->children.Add(newNode);
-		node->parent = root;
+		rootNode = new treeNode<DATA>(dataGiven);
+		rootNode->data = dataGiven;
+		rootNode->parent = NULL;
 	}
 
-	void Add(TYPE dataGiven, tnode<TYPE>* parentGiven)
+	treeNode<DATA>* Add(const DATA& dataGiven)
 	{
-		tnode<TYPE>* newNode = new tnode<TYPE>(dataGiven);
+		treeNode<DATA>* newNode = new treeNode<DATA>(dataGiven);
+		rootNode->children.Add(newNode);
+		newNode->parent = rootNode;
+		return newNode;
+	}
+
+	treeNode<DATA>* Add(DATA dataGiven, treeNode<DATA>* parentGiven)
+	{
+		treeNode<DATA>* newNode = new treeNode<DATA>(dataGiven);
 		parentGiven->children.Add(newNode);
 		newNode->parent = parentGiven;
+		return newNode;
 	}
+
+	//Cerca "Pre-Order: pares->fills"  F B A D C E G I H
+	//		"In-Order:	meitat fills->pare->meitat fill	A B C D E F H I G	
+	//		"Post-Order: fills->pare"	A C E D B H I G F
+
+	void VisitAllNodes_PreorderRecursive(DList<DATA>* list) const
+	{
+		rootNode->VisitAll_PreorderRecursive(list);
+	}
+
+	void VisitAllNodes_PostorderRecursive(DList<DATA>* list) const
+	{
+		rootNode->VisitAll_PostorderRecursive(list);
+	}
+
+	void VisitAllNodes_InorderRecursive(DList<DATA>* list) const
+	{
+		rootNode->VisitAll_InorderRecursive(list);
+	}
+
+
 };
 
 #endif //__TREE_H__
